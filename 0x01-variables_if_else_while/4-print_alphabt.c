fc3d3671777e9b58a2c93cbad308ef4649732ae0
LuhleNtuli <luhlentuli@gmail.com>
2022-06-03 16:24:52 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main -  alphabet in lowercase, followed by a new line
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char i;
+if (i = 'a'; i <= 'z'; i++)
+
+if (i != 'e' && i != 'q')
+putchar(c);
+putchar('\n');
+return (0);
+}
