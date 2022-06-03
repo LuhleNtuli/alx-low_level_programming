87ae8b7e56e61c35d115e3dcd691f588004d528e
LuhleNtuli <luhlentuli@gmail.com>
2022-06-02 17:10:52 -0700

    100-intel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
  /**
-  * main- entry point
+  *main- Entry point
   * Description:main is the first entry point
   * Return: Always 0 (Success)
   */
-int main(void) /* main is the first entry point*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
