098bb63c530d75f1068022597e80a279d9a40e1d
LuhleNtuli <luhlentuli@gmail.com>
2022-06-08 03:40:13 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char j = 'a';
+for (j = 'a'; j <= 'z'; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
